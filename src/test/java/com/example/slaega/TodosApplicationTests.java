ea1b76f1930b04d740d4a25857cdb0f821810133
Seba Gedeon Matsoula Malonga <gedeon@Mac-mini-de-Nanocreatives-4.local>
2024-01-19 13:25:15 +0100

    first commit

---
+++ src/test/java/com/example/slaega/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slaega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
